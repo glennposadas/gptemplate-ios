e157030c1ebec6ab79286aa2dddf2785c6d50401
Glenn Posadas <admin@glennvon.com>
2019-07-15 20:05:48 +0800

    ADD: Project template.

---
+++ GPTemplateiOS/GPTemplateiOS/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  GPTemplateiOS
+//
+//  Created by Glenn Posadas on 12/07/2019.
+//  Copyright © 2019 Glenn Posadas. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
---
+++ GPTemplateiOS/GPTemplateiOSTests/LalaFood_DriverTests.swift
@@ -0,0 +1,34 @@
+//
+//  LalaFood_DriverTests.swift
+//  LalaFood-DriverTests
+//
+//  Created by Glenn Posadas on 12/07/2019.
+//  Copyright © 2019 Glenn Posadas. All rights reserved.
+//
+
+import XCTest
+@testable import LalaFood_Driver
+
+class LalaFood_DriverTests: XCTestCase {
+
+    override func setUp() {
+        // Put setup code here. This method is called before the invocation of each test method in the class.
+    }
+
+    override func tearDown() {
+        // Put teardown code here. This method is called after the invocation of each test method in the class.
+    }
+
+    func testExample() {
+        // This is an example of a functional test case.
+        // Use XCTAssert and related functions to verify your tests produce the correct results.
+    }
+
+    func testPerformanceExample() {
+        // This is an example of a performance test case.
+        self.measure {
+            // Put the code you want to measure the time of here.
+        }
+    }
+
+}
